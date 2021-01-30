9abf058be33a44fca4acc812128ad90491be94f2
Ilton ingui <iltoningui@outlook.com>
2021-01-30 14:22:19 +0100

    Changes response object name
    Changes the response object column status to status code

--- vpos/test/test_vpos.py
+++ vpos/test/test_vpos.py
@@ -6,38 +6,38 @@ class TestVpos:
         merchant = Vpos()
         payment = merchant.new_payment("992563019", "123.45")
 
-        assert payment.get('status') == 202
+        assert payment.get('status_code') == 202
 
     def test_not_create_payment_if_customer_format_is_invalid(self):
         merchant = Vpos()
         payment = merchant.new_payment("99256301", "123.45")
 
-        assert payment.get('status') == 400
+        assert payment.get('status_code') == 400
 
     def test_not_create_a_new_payment_if_amount_format_is_invalid(self):
         merchant = Vpos()
         payment = merchant.new_payment("992563019", "123.45.01")
 
-        assert payment.get('status') == 400
+        assert payment.get('status_code') == 400
 
     def test_should_create_a_new_refund_request_transaction(self):
         merchant = Vpos()
         response = merchant.new_refund("1jYQryG3Qo4nzaOKgJxzWDs25Hv")
 
-        assert response.get('status') == 202
+        assert response.get('status_code') == 202
 
     def test_not_create_new_refund_if_parent_transaction_id_is_blank(self):
         merchant = Vpos()
         response = merchant.new_refund(None)
 
-        assert response.get('status') == 400
+        assert response.get('status_code') == 400
 
     def test__not_create_new_refund_if_supervisor_card_is_invalid(self):
         merchant = Vpos()
         response = merchant.new_refund(
             "1jYQryG3Qo4nzaOKgJxzWDs25Hv", supervisor_card="123123123123123")
 
-        assert response.get('status') == 202
+        assert response.get('status_code') == 202
 
         refund_id = response.get('location')[17:]
         refund_transaction = merchant.get_transaction(refund_id)
@@ -49,19 +49,19 @@ class TestVpos:
         merchant = Vpos()
         response = merchant.get_transactions()
 
-        assert response.get('status') == 200
+        assert response.get('status_code') == 200
 
     def test_should_get_a_single_transaction(self):
         merchant = Vpos()
         response = merchant.get_transaction("1jYQryG3Qo4nzaOKgJxzWDs25Ht")
 
-        assert response.get('status') == 200
+        assert response.get('status_code') == 200
 
     def test_should_not_get_a_non_existent_single_transaction(self):
         merchant = Vpos()
         response = merchant.get_transaction("1jYQryG3Qo4nzaOKgJxzWDs25H")
 
-        assert response.get('status') == 404
+        assert response.get('status_code') == 404
 
     def test_should_get_a_running_single_request_status(self):
         merchant = Vpos()
@@ -69,4 +69,4 @@ class TestVpos:
         refund_id = response.get('location')[17:]
         response = merchant.get_request(refund_id)
 
-        assert response.get('status') == 200
+        assert response.get('status_code') == 200
